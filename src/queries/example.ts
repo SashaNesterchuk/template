c00d0a2d5c48a7005a8d648c50cee53b47fe45ab
Oleksandr Nesterchuk <Oleksandr.Nesterchuk@toshibagcs.com>
2023-10-23 20:20:46 +0200

    Query and util httpRequest

---
+++ src/queries/example.ts
@@ -0,0 +1,6 @@
+import httpRequest from "@/utils/httpRequest";
+
+export const fetchExample = async (): Promise<Array<string>> => {
+    return httpRequest<Array<string>>("GET", "http://localhost:3000/");
+  };
+  
--- src/store/example/thunks.ts
+++ src/store/example/thunks.ts
@@ -1,9 +1,8 @@
-/* Instruments */
+import { fetchExample } from "@/queries/example";
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { fetchData as fetchDataQuery } from "./queries";
 
 export const fetchData = createAsyncThunk("example/fetchData", async () => {
-  const response = await fetchDataQuery();
+  const response = await fetchExample();
 
   return response;
 });
