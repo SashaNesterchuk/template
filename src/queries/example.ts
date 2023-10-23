6b085742a36195497a1f15821470917e6b417dc8
“Oleksandr <sashanesterchuk@sashas-macbook-pro-2.home>
2023-10-23 20:22:33 +0200

    Linter

--- src/queries/example.ts
+++ src/queries/example.ts
@@ -1,6 +1,5 @@
 import httpRequest from "@/utils/httpRequest";
 
 export const fetchExample = async (): Promise<Array<string>> => {
-    return httpRequest<Array<string>>("GET", "http://localhost:3000/");
-  };
-  
+  return httpRequest<Array<string>>("GET", "http://localhost:3000/");
+};
--- src/utils/httpRequest.ts
+++ src/utils/httpRequest.ts
@@ -1,23 +1,23 @@
 async function httpRequest<T>(
-    method: 'GET' | 'POST' | 'PUT' | 'DELETE',
-    url: string,
-    data?: any
-  ): Promise<T> {
-    const requestInit: RequestInit = {
-      method,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: data ? JSON.stringify(data) : undefined,
-    };
-  
-    const response = await fetch(url, requestInit);
-  
-    if (!response.ok) {
-      throw new Error(`HTTP request failed with status: ${response.status}`);
-    }
-  
-    return response.json() as Promise<T>;
+  method: "GET" | "POST" | "PUT" | "DELETE",
+  url: string,
+  data?: any
+): Promise<T> {
+  const requestInit: RequestInit = {
+    method,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: data ? JSON.stringify(data) : undefined,
+  };
+
+  const response = await fetch(url, requestInit);
+
+  if (!response.ok) {
+    throw new Error(`HTTP request failed with status: ${response.status}`);
   }
-  
+
+  return response.json() as Promise<T>;
+}
+
 export default httpRequest;
